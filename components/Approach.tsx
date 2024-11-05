9592a4724151394aad0ff4a23d79b5406054fb3d
mangoshoieb <159851387+mangoshoieb@users.noreply.github.com>
2024-11-05 17:17:33 +0200

    fix the datatype

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -186,7 +186,7 @@ const AceternityIcon = ({ order }: { order: string }) => {
   );
 };
 
-export const Icon = ({ className, ...rest }: React.ReactNode) => {
+export const Icon = ({ className, ...rest }: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
