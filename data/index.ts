663f1a453cecb1416780a00781d020a447fdcd37
mangoshoieb <159851387+mangoshoieb@users.noreply.github.com>
2024-11-05 15:55:08 +0200

    adding new porject

--- data/index.ts
+++ data/index.ts
@@ -82,9 +82,9 @@ export const projects = [
     id: 2,
     title: "Yoom - Video Conferencing App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
+    img: "/promptopia_image.jpg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/zoom-clone",
+    link: "https://promptopia-ten-livid.vercel.app/",
   },
   {
     id: 3,
