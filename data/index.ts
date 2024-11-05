eeb8246ca0c7b9da4365c3a9606076849c7a9385
mangoshoieb <159851387+mangoshoieb@users.noreply.github.com>
2024-11-05 15:43:28 +0200

    edit the portfolio

--- data/index.ts
+++ data/index.ts
@@ -74,9 +74,9 @@ export const projects = [
     id: 1,
     title: "3D Solar System Planets to Explore",
     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
+    img: "/nike_website_image.jpg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "https://github.com/adrianhajdin?tab=repositories",
+    link: "https://simple-nike-webpage.vercel.app/",
   },
   {
     id: 2,
